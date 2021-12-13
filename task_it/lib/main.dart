d8edf5f762b730dcfa00e236744f398a71d8831e
yaraamrsalah <yaraamrsalahh@gmail.com>
2021-12-13 13:01:39 +0200

    updated bottom nav bar

--- task_it/lib/main.dart
+++ task_it/lib/main.dart
@@ -1,2 +1,30 @@
-// import 'defaults/bottom_navigator.dart';
+import 'package:flutter/material.dart';
+import 'package:task_it/screens/homepage.dart';
+import 'package:task_it/utils/custom_colors.dart';
 
+void main() => runApp(TaskItApp());
+
+class TaskItApp extends StatefulWidget {
+  TaskItApp({Key? key}) : super(key: key);
+
+  _TaskItAppState createState() => _TaskItAppState();
+}
+
+class _TaskItAppState extends State<TaskItApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        canvasColor: CustomColors.GreyBackground,
+        fontFamily: 'rubik',
+      ),
+      home: Homepage(),
+    );
+  }
+}
--- task_it/lib/screens/homepage.dart
+++ task_it/lib/screens/homepage.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+import '/utils/custom_colors.dart';
+import '/utils/bottom_navigator.dart';
+
+class Homepage extends StatefulWidget {
+  Homepage({Key? key}) : super(key: key);
+
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final bottomNavBarIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.fromLTRB(20, 0, 20, 15),
+        padding: EdgeInsets.fromLTRB(5, 13, 5, 13),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            stops: [0.015, 0.015],
+            colors: [CustomColors.GreenIcon, Colors.white],
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigator(context, bottomNavBarIndex),
+    );
+  }
+}
