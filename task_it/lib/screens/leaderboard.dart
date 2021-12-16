dfc0a3bda0d4f0e29ae2f3cf58fbfe7791859b4a
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-12-17 00:27:36 +0200

    Update leaderboard.dart

---
+++ task_it/lib/screens/leaderboard.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class LeaderBoardScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // ignore: prefer_const_constructors
+      decoration: BoxDecoration(
+        color: Colors.amber,
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation:0.0,
+          leading: Icon(Icons.arrow_back_ios, color: Colors.white,),
+          actions: [Icon(Icons.grid_view, color: Colors.white,),],
+        ),
+      ),
+    );
+  }
+
+
+}
