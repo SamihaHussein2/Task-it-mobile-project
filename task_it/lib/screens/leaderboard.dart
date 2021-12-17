1ab5809cbb6d01e50262c5c6c9515d4c985f5183
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-12-17 13:26:36 +0200

    Update leaderboard.dart

--- task_it/lib/screens/leaderboard.dart
+++ task_it/lib/screens/leaderboard.dart
@@ -1,5 +1,4 @@
 
-
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
@@ -11,41 +10,46 @@ class LeaderBoardScreen extends StatelessWidget{
     return Container(
       // ignore: prefer_const_constructors
       decoration: BoxDecoration(
-        color: Colors.teal[200],
+      color: Colors.teal[300],
       ),
       child: Scaffold(
+         backgroundColor: Colors.transparent,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation:0.0,
           leading: Icon(Icons.arrow_back_ios, color: Colors.white,),
+          title: Text("Leaderboard"),
+          centerTitle: true,
           actions: [Icon(Icons.grid_view, color: Colors.white,),],
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.8),
-          child: Column(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                height:50.0,
-                decoration:BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  color: Colors.teal[100],
-                ),
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Year', style:TextStyle(color: Colors.white,),),
-                    Text('Week', style:TextStyle(color: Colors.white,),),
-                    Text('Month', style:TextStyle(color: Colors.white,),),
-                  ],
-                )
-              )
+              Classify(), Classify(), Classify()
             ],
-          )
+          ),
+          
         )
       ),
     );
   }
+}
 
-
+class Classify extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 30.0,
+      width:70.0,
+      decoration: BoxDecoration(
+        color: Colors.teal[100],
+        borderRadius: BorderRadius.circular(15.0),
+      )
+    );
+  }
+  
 }
 
