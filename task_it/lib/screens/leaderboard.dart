8751ebd82ca578f35913940f61401cfebb8533c1
FaridaHesham99 <faridamohamed1802642@miuegypt.edu.eg>
2021-12-17 20:46:45 +0200

    Added Leaderboard Images + changed leaderboard.dart file

--- task_it/lib/screens/leaderboard.dart
+++ task_it/lib/screens/leaderboard.dart
@@ -10,31 +10,54 @@ class LeaderBoardScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-      color: Colors.teal[300],
+      color: Colors.white,
       ),
       child: Scaffold(
          backgroundColor: Colors.transparent,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation:0.0,
-          leading: Icon(Icons.arrow_back_ios, color: Colors.white,),
+          leading: Icon(Icons.arrow_back_ios, color: Color(0xFF1E4E5F),),
           title: Text("Leaderboard"),
+          titleTextStyle: TextStyle(color: Color(0xFF1E4E5F),),
           centerTitle: true,
-          actions: [Icon(Icons.grid_view, color: Colors.white,),],
+          
+          actions: [Icon(Icons.grid_view, color: Color(0xFF1E4E5F),),],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.8),
-          child: Row(
+        body: Column(
+          children:[
+          //Row1
+            Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text('Today'),
               Category(),
-              Text('Month')
+              Text('Month'),
+              
             ],
           ),
-          
-        )
+          //Row2
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Crown()
+            ]
+          ),
+          //Row3
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //Text('data'),
+              TopThree()
+            ],
+          )
+          ]
+            
+        ),
+       
       ),
     );
   }
@@ -60,5 +83,57 @@ class Category extends StatelessWidget {
     );
   }
   
+}
+
+//ignore: use_key_in_widget_constructors
+class TopThree extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100.0,
+      width: 100.0,
+      decoration: BoxDecoration(
+        border: Border.all(width: 4, color: Colors.teal),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.teal,
+            spreadRadius: 3,
+            blurRadius: 10,
+          ),
+           BoxShadow(
+            color: Colors.teal,
+            spreadRadius: -3,
+            blurRadius: 5,
+          )
+        ],
+          image: DecorationImage(
+          image: AssetImage(
+              'assets/leaderboard/Top1.jpg'),
+          fit: BoxFit.fill,
+        ),
+       borderRadius: BorderRadius.all(Radius.circular(50))
+      ),
+      //child:Image.asset('assets/leaderboard/Top1.jpg'),
+    );
+  }
+
+}
+
+class Crown extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40.0,
+     width: 50.0,
+        decoration: BoxDecoration(
+           image: DecorationImage(
+          image: AssetImage(
+              'assets/leaderboard/crown.png'),
+          fit: BoxFit.fill,
+        ),
+        )
+    );
+  }
+
 }
 
